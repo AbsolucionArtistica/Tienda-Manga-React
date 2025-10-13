6c2d4ebd8b2372f465e2ddeb322e82eac8d4d9c5
Joaquin Fernandez <joa.fernandezf@duocuc.cl>
2025-10-13 02:16:00 -0300

    Configures base routes and adds account page
    Adjusts the root path to explicitly map to the Home component.
    Introduces an additional `/home` route for the Home component, providing more flexible navigation.
    Adds a new, empty `Cuenta` page, ready for future content.

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,19 @@
-import { BrowserRouter } from 'react-router-dom'
-import Navbar from './components/Navbar'
-import { Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
-import styles from './styles/main.css'
-import Footer from './components/Footer'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Footer from './components/Footer';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
 
 function App() {
-
   return (
     <BrowserRouter>
       <Navbar />
       <Routes>
-        <Route path='' element={<Home />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
       </Routes>
       <Footer />
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
