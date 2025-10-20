61ab4c49e676c95e6ac387b69dcd2efaf607dc4b
Joaquin Fernandez <joa.fernandezf@duocuc.cl>
2025-10-20 11:30:54 -0300

    Update to the original file

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,30 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Footer from './components/Footer';
+import { BrowserRouter } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import Tienda from './pages/Tienda';
+import Producto from './pages/Producto';
+import Checkout from './pages/Checkout';
+import './styles/main.css';
+import Footer from './components/Footer';
+import { CarritoProvider } from './context/CarritoContext';
+import CarritoLateral from './components/CarritoLateral';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/home" element={<Home />} />
-      </Routes>
-      <Footer />
-    </BrowserRouter>
+    <CarritoProvider>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="" element={<Home />} />
+          <Route path="/tienda" element={<Tienda />} />
+          <Route path="/producto/:id" element={<Producto />} />
+          <Route path="/checkout" element={<Checkout />} />
+        </Routes>
+        <Footer />
+        <CarritoLateral />
+      </BrowserRouter>
+    </CarritoProvider>
   );
 }
 
