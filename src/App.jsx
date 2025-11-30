abe03bd394b5e5d3b57f31224a2ad4254f261e96
Joaquin Fernandez <joa.fernandezf@duocuc.cl>
2025-11-30 12:19:35 -0300

    feat(app): Configure authentication, protected routes, and admin paths

--- src/App.jsx
+++ src/App.jsx
@@ -8,28 +8,41 @@ import Home from './pages/Home';
 import Login from './pages/Login';
 import Perfil from './pages/Perfil';
 import Producto from './pages/Producto';
-import Registro from './pages/Registro';
+
 import Tienda from './pages/Tienda';
+import MangaAdminList from './pages/admin/MangaAdminList';
+import MangaAdminForm from './pages/admin/MangaAdminForm';
 import './styles/main.css';
+import { AuthProvider } from './context/AuthContext';
+import ProtectedRoute from './components/ProtectedRoute';
+import Register from './pages/Register';
 
 function App() {
   return (
-    <CarritoProvider>
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path="" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/registro" element={<Registro />} />
-          <Route path="/perfil" element={<Perfil />} />
-          <Route path="/tienda" element={<Tienda />} />
-          <Route path="/producto/:id" element={<Producto />} />
-          <Route path="/checkout" element={<Checkout />} />
-        </Routes>
-        <Footer />
-        <CarritoLateral />
-      </BrowserRouter>
-    </CarritoProvider>
+    <AuthProvider>
+      <CarritoProvider>
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/perfil" element={<Perfil />} />
+            <Route path="/tienda" element={<Tienda />} />
+            <Route path="/producto/:id" element={<Producto />} />
+            <Route path="/checkout" element={<Checkout />} />
+
+            <Route element={<ProtectedRoute role="ADMIN" />}>
+              <Route path="/admin/mangas" element={<MangaAdminList />} />
+              <Route path="/admin/mangas/crear" element={<MangaAdminForm />} />
+              <Route path="/admin/mangas/editar/:id" element={<MangaAdminForm />} />
+            </Route>
+          </Routes>
+          <Footer />
+          <CarritoLateral />
+        </BrowserRouter>
+      </CarritoProvider>
+    </AuthProvider>
   );
 }
 
